dc0b7301475f1380fc638c922a7c5039edaffcdd
Roman Shemshei <roman.shemshei@bluechipsys.com>
2017-06-06 12:57:16 +0300

    Removed commented code

--- app/src/main/java/com/shemshei/simpleredditviewer/App.java
+++ app/src/main/java/com/shemshei/simpleredditviewer/App.java
@@ -11,87 +11,16 @@ import com.shemshei.simpleredditviewer.rest.IDataManager;
 
 public class App extends Application {
 
-//    private Retrofit mRetrofit;
-//    private RedditAPI mApi;
-
     private IDataManager mDataManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
-//        final String authToken = Credentials.basic("reddit_simple_reader", "hHFxuoK11Br8zA");
-//        final String authToken = Credentials.basic("hHFxuoK11Br8zA", "");
-//        final String authToken = Credentials.basic("reddit_simple_reader", "");
-
-//        createRetrofit(authToken);
-
 
         mDataManager = DataManagerFactory.createDataManager(DataManagerFactory.SOURCE_REDDIT, getApplicationContext());
     }
 
-//    public RedditAPI getApi() {
-//        return mApi;
-//    }
-
     public IDataManager getDataManager(){
         return mDataManager;
     }
-
-//    public void createRetrofit(String token){
-//        if(mRetrofit != null){
-//            mRetrofit = null;
-//        }
-//
-//        AuthenticationInterceptor interceptor =
-//                new AuthenticationInterceptor(token);
-//
-//        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-//
-//        if (!httpClient.interceptors().contains(interceptor)) {
-//            httpClient.addInterceptor(interceptor);
-//        }
-//
-//        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-//        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-//
-//        if (!httpClient.interceptors().contains(loggingInterceptor)) {
-//            httpClient.addInterceptor(loggingInterceptor);
-//        }
-//
-//
-//        mRetrofit = new Retrofit.Builder()
-//                .baseUrl("https://oauth.reddit.com/")
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .client(httpClient.build())
-//                .build();
-//
-//        mApi = mRetrofit.create(RedditAPI.class);
-//    }
-
-//    public class AuthenticationInterceptor implements Interceptor {
-//
-//        private String authToken;
-//
-//        public AuthenticationInterceptor(String token) {
-//            this.authToken = token;
-//        }
-//
-//        @Override
-//        public Response intercept(Chain chain) throws IOException {
-//            Request request = chain.request();
-//            Request authenticatedRequest = request.newBuilder()
-//                    .header("Authorization", authToken)
-//                    .header("Accept", "application/json").build();
-//            return chain.proceed(authenticatedRequest);
-//
-//
-////            Request original = chain.request();
-////
-////            Request.Builder builder = original.newBuilder()
-////                    .header("Authorization", authToken);
-////
-////            Request request = builder.build();
-////            return chain.proceed(request);
-//        }
-//    }
 }
--- app/src/main/java/com/shemshei/simpleredditviewer/rest/RedditAPI.java
+++ app/src/main/java/com/shemshei/simpleredditviewer/rest/RedditAPI.java
@@ -1,16 +1,11 @@
 package com.shemshei.simpleredditviewer.rest;
 
-import java.util.Map;
-
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
-import retrofit2.http.QueryMap;
-import retrofit2.http.Url;
 
 /**
  * Created by roma on 29.05.17.
@@ -27,16 +22,5 @@ interface RedditAPI {
 
     @GET("/top")
     Call<ListingResponse> getTopList(@Query("after") String after, @Query("limit") int limit);
-
-    @GET
-    Call<String> authorize(@Url String url, @QueryMap Map<String, String> map);
-
-    @GET
-    Call<String> authorize(@Url String url,
-                           @Query("client_id") String clientId,
-                           @Query("response_type") String ResponseType,
-                           @Query("state") String state,
-                           @Query("redirect_uri") String redirectUri,
-                           @Query("scope") String scope);
 
 }
